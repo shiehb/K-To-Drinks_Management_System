8b3c30e843b0c13fe2c4a3677ce781d22b23ac56
JERICHO URBANO <119677296+shiehb@users.noreply.github.com>
2025-03-25 19:40:58 +0800

    updated

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000'; // Your Django backend URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 const authService = {
   // Login function
