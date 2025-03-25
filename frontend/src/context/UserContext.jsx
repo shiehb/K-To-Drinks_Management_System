71a489dfdf62f628ae5c29f961fa7f4a13d00d07
JERICHO URBANO <119677296+shiehb@users.noreply.github.com>
2025-03-25 19:22:41 +0800

    1

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect } from "react";
 
-const API_URL = `${import.meta.env.VITE_API_URL}/api` || 'http://localhost:8000/api';
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 
 export const UserContext = createContext();
