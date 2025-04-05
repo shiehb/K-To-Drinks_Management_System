c0292488f6207fba993ab3be4e62601b67714ab1
JERICHO URBANO <119677296+shiehb@users.noreply.github.com>
2025-04-05 19:56:45 +0800

    Update api_url.js

--- frontend/src/api/api_url.js
+++ frontend/src/api/api_url.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://k-to-drinks-management-system.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
